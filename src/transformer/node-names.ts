db552738b30877bdc205c5877849812ee87336eb
Alf Eaton <aeaton@atypon.com>
2020-05-14 13:23:10 +0100

    Add node names for more section types

--- src/transformer/node-names.ts
+++ src/transformer/node-names.ts
@@ -36,4 +36,6 @@ export const nodeNames: Map<ManuscriptNodeType, string> = new Map([
   [schema.nodes.table_element, 'Table'],
   [schema.nodes.blockquote_element, 'Block Quote'],
   [schema.nodes.pullquote_element, 'Pull Quote'],
+  [schema.nodes.keywords_section, 'Section'],
+  [schema.nodes.toc_section, 'Section'],
 ])
